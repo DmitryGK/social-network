62f70dae33b464e85b598b55d5dc6594786715b4
DmitryGK <gulyakevichdmitry@gmail.com>
2022-01-06 21:45:51 +0300

    add grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React from 'react'
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+import logo from './Pictures/Logo.png'
+import pic from './Pictures/wp.jpg'
 
 const App = () => {
   return (
-      <div>
-          <Header />
-          <Technologies />
-          <Footer />
+      <div className='app-wrapper'>
+          <header className='header'>
+                <img src={logo} alt={logo}/>
+          </header>
+          <nav className='nav'>
+              <div>
+                  <a>Profile</a>
+              </div>
+              <div>
+                  <a>Messages</a>
+              </div>
+              <div>
+                  <a>News</a>
+              </div>
+              <div>
+                  <a>Settings</a>
+              </div>
+
+          </nav>
+          <div className='content'>
+              <div ><img className='image' src={pic} alt={pic}/></div>
+              <div>
+                  Ava + description
+              </div>
+              <div>
+                  My posts
+                  <div>
+                      New post
+                  </div>
+                  <div>
+                      <div>
+                          post 1
+                      </div>
+                      <div>
+                          post 2
+                      </div>
+                  </div>
+              </div>
+          </div>
+
       </div>
+
   );
 }
 
